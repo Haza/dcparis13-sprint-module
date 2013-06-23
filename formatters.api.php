d3b5f300e154f68fa44c3b014857ac8756f2e17a
lisa liaz <lisaliaz@gmail.com>
2013-06-23 14:48:32 +0000

    first api commit

---
+++ formatters.api.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Formatters module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * @todo Function description
+ *
+ * @return
+ *   An array of formatters.
+ */
+function hook_formatters_component_info() {
+  $components = array();
+
+  return $components;
+}
+
+/**
+ * @todo Function description
+ *
+ * @return
+ *   An array of formatters.
+ */
+function hook_formatters_component_info_alter(&$components) {
+  // @todo Fix this
+  $components['my_simple_components']['settings']['length'] = 150;
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
